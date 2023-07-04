f823a9eb0efe4d556f02c452665f07951d5d5b7b
Peally <pieclandre@gmail.com>
2023-07-04 20:57:03 +0200

    feat : hydrate app with store

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Provider } from "react-redux";
 import Router from "./Router";
+import store from "./utils/store/store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Router />
-    </>
+    </Provider>
   );
 }
 
