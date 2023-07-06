c38ee527a68e3e88479f629363034361a9b5d565
Peally <pieclandre@gmail.com>
2023-07-06 23:55:48 +0200

    feat : add employee data template

---
+++ src/utils/data/employeeTemplate.js
@@ -0,0 +1,13 @@
+const employeeTemplate = {
+  firstName: null,
+  lastName: null,
+  startDate: null,
+  dateOfBirth: null,
+  street: null,
+  city: null,
+  state: null,
+  zipCode: null,
+  department: null,
+};
+
+export default employeeTemplate;
