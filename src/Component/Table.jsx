b61564b8d363ace9f7bfe7dd95b1865599f09a65
Peally <pieclandre@gmail.com>
2023-07-06 23:54:08 +0200

    feat : custom table with employee data and sort

---
+++ src/Component/Table.jsx
@@ -0,0 +1,104 @@
+import { useSelector } from "react-redux";
+
+import employeeTemplate from "../utils/data/employeeTemplate";
+import { useState } from "react";
+import { useEffect } from "react";
+import EmployeeSortAndFormatService from "../utils/services/employee";
+import { v4 as uuidv4 } from "uuid";
+import Pagination from "./Pagination";
+import CustomSearch from "./CustomSearch";
+
+const Table = () => {
+  const service = new EmployeeSortAndFormatService();
+
+  const employees = useSelector((state) => state.employee);
+
+  const employeesKeys = Object.keys(employeeTemplate);
+
+  const [filter, setFilter] = useState({
+    type: null,
+    isDesc: true,
+    text: "",
+  });
+  const [filteredEmployees, setFilteredEmployees] = useState(
+    service.formatDate(employees)
+  );
+
+  const [numberPerPage, setNumberPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    if (filter.type || filter.text) {
+      setFilteredEmployees(
+        service.sortEmployees(
+          employees,
+          filter.type,
+          filter.isDesc,
+          filter.text.toLowerCase()
+        )
+      );
+    } else {
+      setFilteredEmployees(service.formatDate(employees));
+    }
+  }, [filter]);
+
+  return (
+    <>
+      <div className="tools">
+        <CustomSearch
+          numberPerPage={numberPerPage}
+          setNumberPerPage={setNumberPerPage}
+          filter={filter}
+          setFilter={setFilter}
+        />
+        <Pagination
+          numberPerPage={numberPerPage}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+          filteredEmployees={filteredEmployees}
+        />
+      </div>
+      <div
+        className="table"
+        style={{ gridTemplateColumns: `repeat(${employeesKeys.length}, auto)` }}
+      >
+        {employeesKeys.map((key) => (
+          <div
+            className="table__content table__head"
+            key={key}
+            onClick={() =>
+              setFilter(
+                filter.type === key
+                  ? { ...filter, isDesc: !filter.isDesc }
+                  : { ...filter, type: key, isDesc: true }
+              )
+            }
+          >
+            <span>{service.formatKey(key)}</span>
+          </div>
+        ))}
+
+        {filteredEmployees.length > 0 ? (
+          filteredEmployees
+            .slice(
+              (currentPage - 1) * numberPerPage,
+              currentPage * numberPerPage
+            )
+            .map((user) =>
+              employeesKeys.map((key) => (
+                <div className="table__content table__body" key={uuidv4()}>
+                  <span>{user[key]}</span>
+                </div>
+              ))
+            )
+        ) : (
+          <div className="table__content--no-employee">
+            <span>No employees</span>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Table;
