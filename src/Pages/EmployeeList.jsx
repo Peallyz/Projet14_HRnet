4fbbf10e41b4d5afb7f86e83e472dfc51f8b70ad
Peally <pieclandre@gmail.com>
2023-07-08 00:46:40 +0200

    feat : add successfully populate modal

--- src/Pages/EmployeeList.jsx
+++ src/Pages/EmployeeList.jsx
@@ -3,17 +3,33 @@ import Header from "../Component/Header";
 import Link from "../Component/Link";
 import Table from "../Component/Table";
 import { populateStore } from "../utils/slice/employee.slice";
+import CustomModal from "custom-modal-component/dist/CustomModal";
+import { useState } from "react";
 
 const EmployeeList = () => {
   const dispatch = useDispatch();
+
+  const [showCustomModal, setShowCustomModal] = useState(false);
 
   return (
     <>
       <Header title="Current Employees" />
       <main>
+        <CustomModal
+          showCustomModal={showCustomModal}
+          setShowCustomModal={setShowCustomModal}
+        >
+          <h2>You&apos;ve succesfully populate store with employees</h2>
+        </CustomModal>
         <div className="button__container">
           <Link destination="/" content="Retour à l'acceuil" />
-          <button className="link" onClick={() => dispatch(populateStore())}>
+          <button
+            className="link"
+            onClick={() => {
+              setShowCustomModal(true);
+              dispatch(populateStore());
+            }}
+          >
             Populate Store
           </button>
         </div>
