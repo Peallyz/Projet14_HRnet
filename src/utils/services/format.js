b0daf0b2c461b9d3023463b5b87e3b6d03c16044
Peally <pieclandre@gmail.com>
2023-07-08 00:07:46 +0200

    doc: add JS Doc

--- src/utils/services/format.js
+++ src/utils/services/format.js
@@ -1,3 +1,9 @@
+/**
+ * Formats the date properties for each employee to a human-readable format.
+ *
+ * @param {Array} employees - An array of employee objects.
+ * @returns {Array} - The formatted array of employees with date properties in localized string format.
+ */
 export const formatDate = (employees) => {
   const formattedEmployees = employees.map((employee) => {
     return {
@@ -8,6 +14,13 @@ export const formatDate = (employees) => {
   });
   return formattedEmployees;
 };
+
+/**
+ * Formats the given key to a human-readable format.
+ *
+ * @param {string} key - The key to format.
+ * @returns {string} - The formatted key.
+ */
 export const formatKey = (key) => {
   switch (key) {
     case "firstName":
@@ -33,35 +46,32 @@ export const formatKey = (key) => {
   }
 };
 
+/**
+ * Returns the data type of the given key.
+ *
+ * @param {string} key - The key to determine the data type for.
+ * @returns {string} - The data type of the key.
+ */
 export const stringOrInt = (key) => {
   switch (key) {
     case "firstName":
       return "string";
-
     case "lastName":
       return "string";
-
     case "dateOfBirth":
       return "int";
-
     case "startDate":
       return "int";
-
     case "street":
       return "string";
-
     case "city":
       return "string";
-
     case "state":
       return "string";
-
     case "zipCode":
       return "int";
-
     case "department":
       return "string";
-
     default:
       return key;
   }
--- src/utils/services/sortAndFilter.js
+++ src/utils/services/sortAndFilter.js
@@ -1,5 +1,18 @@
 import { formatDate, stringOrInt } from "./format";
 
+/**
+ * Sorts the array of employees based on the provided sorting parameters.
+ * If sortByType is specified and sortByText is empty, the employees will be sorted by type and formatted dates will be returned.
+ * If sortByType is not specified and sortByText is provided, the employees will be filtered by text and formatted dates will be returned.
+ * If both sortByType and sortByText are specified, the employees will be sorted by type, then filtered by text, and formatted dates will be returned.
+ * If none of the parameters are specified, the employees will be sorted by the default format and formatted dates will be returned.
+ *
+ * @param {Array} employees - An array of employee objects.
+ * @param {string} sortByType - The type to sort the employees by (e.g., "firstName", "lastName", "dateOfBirth").
+ * @param {boolean} isDesc - Specifies whether to sort in descending order.
+ * @param {string} sortByText - The text to filter the employees by.
+ * @returns {Array} - The sorted and/or filtered array of employees with formatted dates.
+ */
 export const sortEmployees = (employees, sortByType, isDesc, sortByText) => {
   const formattedEmployees = [...employees];
 
@@ -14,13 +27,20 @@ export const sortEmployees = (employees, sortByType, isDesc, sortByText) => {
 
   if (sortByType && sortByText !== "") {
     sortEmployeesByType(formattedEmployees, sortByType, isDesc);
-
     return filterEmployeeByText(formatDate(formattedEmployees), sortByText);
   }
 
-  return this.formatDate(formattedEmployees);
+  return formatDate(formattedEmployees);
 };
 
+/**
+ * Sorts the array of employees by the specified type and in the specified direction.
+ *
+ * @param {Array} employees - An array of employee objects.
+ * @param {string} sortBy - The type to sort the employees by (ex: "firstName", "lastName", "dateOfBirth").
+ * @param {boolean} isDesc - Specifies whether to sort in descending order.
+ * @returns {Array} The sorted array of employees by sors types.
+ */
 export const sortEmployeesByType = (employees, sortBy, isDesc) => {
   employees.sort((a, b) => {
     if (!isDesc) {
@@ -41,6 +61,13 @@ export const sortEmployeesByType = (employees, sortBy, isDesc) => {
   });
 };
 
+/**
+ * Filters the array of employees based on the provided text.
+ *
+ * @param {Array} employees - An array of employee objects.
+ * @param {string} text - The text to filter the employees by.
+ * @returns {Array} - The filtered array of employees.
+ */
 export const filterEmployeeByText = (employees, text) => {
   const filteredEmployee = employees.filter((object) => {
     for (let key in object) {
